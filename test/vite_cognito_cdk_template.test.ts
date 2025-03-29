820c16788083e5478f58fdb964a32fd80054215c
miguel-merlin <mmerlin@stevens.edu>
2025-03-29 18:14:43 -0400

    Initial commit

---
+++ test/vite_cognito_cdk_template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ViteCognitoCdkTemplate from '../lib/vite_cognito_cdk_template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vite_cognito_cdk_template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ViteCognitoCdkTemplate.ViteCognitoCdkTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
